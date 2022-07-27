60679185ea6a4a3b59fdc2472ee45f624ea16cf5
retsambew <namanaskn2@gmail.com>
2022-07-28 00:01:33 +0530

    home page v1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-/* 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './components/home'
+/* 
 import Game from './components/game'
 import GameOver from './components/gameover'
 */
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-      {/* 
+    <Router>
       <Home/>
+      {/* 
       <Game/>
       <GameOver/>
       */}
-    </div>
+    </Router>
   );
 }
 
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styles from '../../styles/home.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.imgSection}><div className={styles.bgImg}/></div>
+      <div className={styles.score}>
+        <h2>Hi-Score: 123</h2>
+      </div>
+      <figure className={styles.planet1}/>
+      <figure className={styles.planet2}/>
+      <figure className={styles.threeCraft}/>
+      <div className={styles.menu}>
+        <Link to='/play' className={styles.menuOption}><h2>Play</h2></Link>
+        <hr/>
+        <Link to='/help' className={styles.menuOption}><h2>Help</h2></Link>
+        <hr/>
+        <Link to='/credits' className={styles.menuOption}><h2>Credits</h2></Link>
+      </div>
+    </div>
   )
 }
 
