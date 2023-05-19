7f2682d81fb32e2c5e5b449262517d6aff661e62
retsambew <namanaskn2@gmail.com>
2023-05-19 11:18:29 +0530

    shoot functionality

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,37 +1,52 @@
-import React, { useEffect, useRef } from 'react'
-import styles from '../../styles/game.module.css';
+import React, { useEffect, useRef, useState } from "react";
+import styles from "../../styles/game.module.css";
 
 const Game = () => {
-  const player=useRef(null)
-  const playerLazer=useRef(null)
+  const player = useRef(null);
+  const playerLazer = useRef(null);
+  const [flag, setFlag] = useState(true);
   const Controls = (event) => {
-    const offset=10; //pixel
-    var playerObject=player.current.getBoundingClientRect();
-    if(event.key === 'ArrowLeft' && playerObject.left>0){
-      player.current.style.left=playerObject.left-offset+"px";
+    const offset = 10; //pixel
+    var playerObject = player.current.getBoundingClientRect();
+    if (event.key === "ArrowLeft" && playerObject.left > 0) {
+      player.current.style.left = playerObject.left - offset + "px";
+    } else if (
+      event.key === "ArrowRight" &&
+      playerObject.right < window.innerWidth
+    ) {
+      player.current.style.left = playerObject.left + offset + "px";
+    } else if (event.key === " ") {
+      if (flag) {
+        setFlag(false);
+        shoot();
+      }
     }
-    else if(event.key === 'ArrowRight' && playerObject.right<window.innerWidth){
-      player.current.style.left=playerObject.left+offset+"px";
-    }
-    else if(event.key === ' '){
-      // shoot
-    }
-  }
+  };
+  const shoot = () => {
+    playerLazer.current.className = styles.shoot;
+    setTimeout(() => {
+      playerLazer.current.className = "";
+      setFlag(true);
+    }, 1000);
+  };
   useEffect(() => {
-    document.addEventListener('keydown',Controls)
-  }, [])
-  
+    document.addEventListener("keydown", Controls);
+  }, []);
+
   return (
     <div>
       <div className={styles.gameArea}>
         <div>
-          <div id={styles.player} ref={player}/>
-          <div className={styles.playerLaserLeft} />
-          <div className={styles.playerLaserRight} />
+          <div id={styles.player} ref={player}>
+            <div id={styles.laser} ref={playerLazer}>
+              <div className={styles.playerLaserLeft} />
+              <div className={styles.playerLaserRight} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
