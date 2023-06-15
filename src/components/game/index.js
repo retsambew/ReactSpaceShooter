7a3305b992ff5d3a304a490b07b9541433f58602
retsambew <namanaskn2@gmail.com>
2023-06-15 18:24:05 +0530

    smoother controls

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -4,44 +4,48 @@ import styles from "../../styles/game.module.css";
 const Game = () => {
   const player = useRef(null);
   const playerLazer = useRef(null);
-  const [flag, setFlag] = useState(true);
+  // const [flag, setFlag] = useState(true);
+  let keysPressed = {};
   const Controls = (event) => {
+    keysPressed[event.key] = true;
     const offset = 10; //pixel
     var playerObject = player.current.getBoundingClientRect();
-    if (event.key === "ArrowLeft" && playerObject.left > 0) {
+    if (keysPressed["ArrowLeft"] && playerObject.left > 0) {
       player.current.style.left = playerObject.left - offset + "px";
-    } else if (
-      event.key === "ArrowRight" &&
-      playerObject.right < window.innerWidth
-    ) {
+    }
+    if (keysPressed["ArrowRight"] && playerObject.right < window.innerWidth) {
       player.current.style.left = playerObject.left + offset + "px";
-    } else if (event.key === " ") {
-      if (flag) {
-        setFlag(false);
-        shoot();
-      }
+    }
+    if (playerLazer.current.className === "" && keysPressed[" "]) {
+      shoot();
     }
   };
   const shoot = () => {
+    // setFlag(false);
     playerLazer.current.className = styles.shoot;
+    playerLazer.current.style.left =
+      player.current.getBoundingClientRect().left + "px";
     setTimeout(() => {
       playerLazer.current.className = "";
-      setFlag(true);
-    }, 1000);
+      // setFlag(true);
+    }, 550);
   };
   useEffect(() => {
     document.addEventListener("keydown", Controls);
+    document.addEventListener("keyup", (event) => {
+      delete keysPressed[event.key];
+    });
   }, []);
 
   return (
+    // <div onClick={() => flag && shoot()}>
     <div>
       <div className={styles.gameArea}>
         <div>
-          <div id={styles.player} ref={player}>
-            <div id={styles.laser} ref={playerLazer}>
-              <div className={styles.playerLaserLeft} />
-              <div className={styles.playerLaserRight} />
-            </div>
+          <div id={styles.player} ref={player}></div>
+          <div id={styles.laser} ref={playerLazer}>
+            <div className={styles.playerLaserLeft} />
+            <div className={styles.playerLaserRight} />
           </div>
         </div>
       </div>
